7ad55b22476d6bcc04b66f86a329a7d797432ca9
jt31981 <jevin.talaviya@stud.th-deg.de>
2026-05-09 02:09:47 +0200

    schemas for order

---
+++ app/schemas/order_schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import List
+
+class OrderItem(BaseModel):
+    product_id: int
+    quantity: int
+
+class OrderCreate(BaseModel):
+    user_id: int
+    items: List[OrderItem]
